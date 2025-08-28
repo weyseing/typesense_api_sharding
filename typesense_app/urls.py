15065709472259065b49e3788045de6a972d2e95
Jeremy Heng <hengweyseing53@gmail.com>
2025-08-28 17:26:01 +0800

    [done] update url

--- typesense_app/urls.py
+++ typesense_app/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('status_count_mins',views.status_count_mins),
     path('transaction',views.transaction),
     path('health',views.healthcheck)
 ]
